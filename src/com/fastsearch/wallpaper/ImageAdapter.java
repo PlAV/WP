3152c3a138aefef9ae73a1bf38cd864574061f3f
Alex Plekhov <aleks.plekhov@gmail.com>
2013-09-12 16:04:01 +0400

    images in GridView

--- src/com/fastsearch/wallpaper/ImageAdapter.java
+++ src/com/fastsearch/wallpaper/ImageAdapter.java
@@ -2,13 +2,17 @@ package com.fastsearch.wallpaper;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,25 +22,34 @@ import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
 	private static final String TAG = "ImageManager";
-	private Context mContext;
+	private final Context mContext;
+	ArrayList<Bitmap> bitmapArray;
+	final static String DEBUG_TAG = "+++ImageDownloader+++";
+	ArrayList<Bitmap> mThumbIds;
 
-	public ImageAdapter(Context c) {
+	public ImageAdapter(Context c, ArrayList<Bitmap> result) {
 		mContext = c;
+		this.mThumbIds = result;
+		// new DownloadImageTask().execute(urls);
 	}
 
+	@Override
 	public int getCount() {
-		return mThumbIds.length;
+		return mThumbIds.size();
 	}
 
+	@Override
 	public Object getItem(int position) {
-		return mThumbIds[position];
+		return mThumbIds.get(position);
 	}
 
+	@Override
 	public long getItemId(int position) {
 		return 0;
 	}
 
 	// create a new ImageView for each item referenced by the Adapter
+	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ImageView imageView;
 		if (convertView == null) { // if it's not recycled, initialize some
@@ -49,28 +62,87 @@ public class ImageAdapter extends BaseAdapter {
 			imageView = (ImageView) convertView;
 		}
 
-		Bitmap image = downloadImage(mThumbIds[position]);
-		if ( image != null ) {
-	          Log.v(TAG, "Got image by URL: " + mThumbIds[position]);
-	          imageView.setImageBitmap(image);
-		}
-		
-		//imageView.setImageResource(mThumbIds[position]);
+		// Bitmap image = downloadImage(mThumbIds[position]);
+		// if (image != null) {
+		Log.v(TAG, "Got image by URL: " + mThumbIds.get(position));
+		// imageView.setImageResource(mThumbIds[position]);
+		// imageView.setImageBitmap(image);
+		// }
+
+		imageView.setImageBitmap(mThumbIds.get(position));
+		// imageView.setImageResource(mThumbIds[position]);
 		return imageView;
 	}
 
 	// references to our images
-	private String[] mThumbIds = {
-			"http://s.4pda.to/forum//uploads/av-2533711-21696238.jpg",
-		 };
 
-	public static Bitmap downloadImage(String iUrl) {
+	class DownloadImageTask extends AsyncTask<String, Void, ArrayList<Bitmap>> {
+		@Override
+		protected ArrayList<Bitmap> doInBackground(String... params) {
+			publishProgress(new Void[] {});
+
+			bitmapArray = new ArrayList<Bitmap>();
+			Integer count = 0;
+			for (Integer i = 0; i < params.length; i++) {
+				count++;
+				InputStream input = null;
+				try {
+					URL urlConn = new URL(params[i]);
+					input = urlConn.openStream();
+					Bitmap img = BitmapFactory.decodeStream(input);
+					bitmapArray.add(img);
+				} catch (MalformedURLException e) {
+					Log.d(DEBUG_TAG, "Oops, Something wrong with URL...");
+					e.printStackTrace();
+				} catch (IOException e) {
+					Log.d(DEBUG_TAG,
+							"Oops, Something wrong with inpur stream...");
+					e.printStackTrace();
+				}
+
+			}
+			//
+
+			return bitmapArray;
+		}
+
+		protected void onPreExecute(ProgressDialog progress) {
+			progress = new ProgressDialog(mContext);
+
+			progress.setMessage("Загрузка каталога...");
+			progress.setIndeterminate(true);
+			progress.setCancelable(true);
+			progress.show();
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			super.onProgressUpdate(values);
+			// showDialog(PROGRESS_DLG_ID);
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<Bitmap> result) {
+			super.onPostExecute(result);
+
+			/*
+			 * for (Integer i = 0; i < result.size(); i++) {
+			 * 
+			 * img1.setImageBitmap(result.get(i));
+			 * img2.setImageBitmap(result.get(i));
+			 * img3.setImageBitmap(result.get(i)); }
+			 */
+
+		}
+	}
+
+	public static Bitmap downloadImage(String mThumbIds2) {
 		Bitmap bitmap = null;
 		HttpURLConnection conn = null;
 		BufferedInputStream buf_stream = null;
 		try {
-			Log.v(TAG, "Starting loading image by URL: " + iUrl);
-			conn = (HttpURLConnection) new URL(iUrl).openConnection();
+			Log.v(TAG, "Starting loading image by URL: " + mThumbIds2);
+			conn = (HttpURLConnection) new URL(mThumbIds2).openConnection();
 			conn.setDoInput(true);
 			conn.setRequestProperty("Connection", "Keep-Alive");
 			conn.connect();
@@ -81,9 +153,9 @@ public class ImageAdapter extends BaseAdapter {
 			buf_stream = null;
 			conn = null;
 		} catch (MalformedURLException ex) {
-			Log.e(TAG, "Url parsing was failed: " + iUrl);
+			Log.e(TAG, "Url parsing was failed: " + mThumbIds2);
 		} catch (IOException ex) {
-			Log.d(TAG, iUrl + " does not exists");
+			Log.d(TAG, mThumbIds2 + " does not exists");
 		} catch (OutOfMemoryError e) {
 			Log.w(TAG, "Out of memory!!!");
 			return null;
--- src/com/fastsearch/wallpaper/MainActivity.java
+++ src/com/fastsearch/wallpaper/MainActivity.java
@@ -1,30 +1,232 @@
 package com.fastsearch.wallpaper;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnItemClickListener {
 
-	String[] data = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k" };
-
 	GridView gvMain;
 	ArrayAdapter<String> adapter;
+	ImageView img1, img2, img3, img4, img5;
+
+	final int PROGRESS_DLG_ID = 666;
+	final static String DEBUG_TAG = "+++ImageDownloader+++";
+	ArrayList<Bitmap> bitmapArray;
+	GridView gridview;
+
+	String[] urls = { "http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png",
+			"http://huuah.com/images/progress2.png", };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		GridView gridview = (GridView) findViewById(R.id.gridview);
-		ImageAdapter adapter = new ImageAdapter(this);
-		gridview.setAdapter(adapter);
-		gridview.setOnItemClickListener(this);
+		gridview = (GridView) findViewById(R.id.gridview);
 
+		new DownloadImageTask().execute(urls);
+
+	}
+
+	class DownloadImageTask extends AsyncTask<String, Void, ArrayList<Bitmap>> {
+		@Override
+		protected ArrayList<Bitmap> doInBackground(String... params) {
+			publishProgress(new Void[] {});
+
+			bitmapArray = new ArrayList<Bitmap>();
+			Integer count = 0;
+			for (Integer i = 0; i < params.length; i++) {
+				count++;
+				InputStream input = null;
+				try {
+					URL urlConn = new URL(params[i]);
+					input = urlConn.openStream();
+					Bitmap img = BitmapFactory.decodeStream(input);
+					bitmapArray.add(img);
+				} catch (MalformedURLException e) {
+					Log.d(DEBUG_TAG, "Oops, Something wrong with URL...");
+					e.printStackTrace();
+				} catch (IOException e) {
+					Log.d(DEBUG_TAG,
+							"Oops, Something wrong with inpur stream...");
+					e.printStackTrace();
+				}
+
+			}
+			//
+
+			return bitmapArray;
+		}
+
+		protected void onPreExecute(ProgressDialog progress) {
+			progress = new ProgressDialog(MainActivity.this);
+
+			progress.setMessage("Загрузка каталога...");
+			progress.setIndeterminate(true);
+			progress.setCancelable(true);
+			progress.show();
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			super.onProgressUpdate(values);
+			// showDialog(PROGRESS_DLG_ID);
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<Bitmap> result) {
+			super.onPostExecute(result);
+
+			ImageAdapter adapter = new ImageAdapter(MainActivity.this, result);
+			gridview.setAdapter(adapter);
+			gridview.setOnItemClickListener(MainActivity.this);
+
+			/*
+			 * for (Integer i = 0; i < result.size(); i++) {
+			 * 
+			 * img1.setImageBitmap(result.get(i));
+			 * img2.setImageBitmap(result.get(i));
+			 * img3.setImageBitmap(result.get(i)); }
+			 */
+			// dismissDialog(PROGRESS_DLG_ID);
+			// Log.d("DDD", "DDD" + result);
+			// img.setImageBitmap(resulti);
+		}
 	}
 
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
